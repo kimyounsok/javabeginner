4cdb5344812bd0f17ba66a1e147dd69318a42480
ost001644 <ost001644@ost001644>
2020-01-06 10:28:00 +0900

    initial commit

---
+++ src/first/exam01.java
@@ -0,0 +1,15 @@
+package first;
+
+import java.util.Scanner;
+
+public class exam01 {
+	
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		
+
+		boolean isFun =true;
+		System.out.println(isFun);
+		
+	}
+}
